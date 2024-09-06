d7b56895a4e83a7401d9bc45cafae1c740ddccef
pabpereza@gmail.com <pabloperezaradros@gmail.com>
2024-09-06 14:23:56 +0200

    Refactor database connection and models configuration

--- app/main.py
+++ app/main.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
 
+# Importing the routers and models
 from .models import quote
 from .routers import quote
 
+# Database Connection
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
 app = FastAPI()
 
+
+# ROUTES 
 app.include_router(quote.router)
 
 
--- app/models/quote.py
+++ app/models/quote.py
@@ -6,16 +6,29 @@ class Source(BaseModel):
     name: str
     year: int
 
+    class config:
+        orm_mode = True
+
 class Type(BaseModel):
     name: str
     description: str
 
+    class config:
+        orm_mode = True
+    
+
 class Tag(BaseModel):
     name: str
     description: str
+
+    class config:
+        orm_mode = True
 
 class Quote(BaseModel):
     text: str
     source: Source | None = None
     type: Type | None = None
     tags: list[Tag] | None = None
+
+    class config:
+        orm_mode = True
