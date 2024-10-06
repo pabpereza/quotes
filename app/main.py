56d325bb5b469139a975d9f5c8164e12cf139958
pabpereza <pabloperezaradros@gmail.com>
2024-10-06 12:15:13 +0200

    New docker-compose version

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import os
 
 # Importing the routers and models
 from .models import quote
@@ -9,12 +10,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-USER = "user"
-PASSWORD = "password"
-HOST = "localhost"
-DATABASE = "quotes"
+# Get variables from environment
+USER = os.getenv("POSTGRES_USER")
+PASSWORD = os.getenv("POSTGRES_PASSWORD")
+HOST = os.getenv("POSTGRES_HOST")
+DATABASE = os.getenv("POSTGRES_DB")
 
-SQLALCHEMY_DATABASE_URL = "postgresql://%s:%s@%s/%s".format(USER, PASSWORD, HOST)
+# Check if the variables are set
+if not USER:
+    raise ValueError("POSTGRES_USER is not set")
+if not PASSWORD:
+    raise ValueError("POSTGRES_PASSWORD is not set")
+if not HOST:
+    raise ValueError("POSTGRES_HOST is not set")
+if not DATABASE:
+    raise ValueError("POSTGRES_DB is not set")
+
+SQLALCHEMY_DATABASE_URL = "postgresql://%s:%s@%s/%s".format(USER, PASSWORD, HOST, DATABASE)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
--- dockerfile
+++ dockerfile
@@ -1,23 +1,13 @@
 FROM python:3.11-slim
 
-
-# Set the working directory
 WORKDIR /app
 
-# Install postgresql-client
-RUN apt-get update && apt-get install -y libpq-dev
+RUN apt update && apt install -y libpq-dev
 
-# Copy requirements file
 COPY requirements.txt .
 
-# Install dependencies
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Copy the current directory contents into the container at /app
 COPY . .
 
-
-# Run the application
-CMD ["fastapi", "run", "app/main.py", "--port", "80"]
-
-
+CMD ["fastapi", "run", "app/main.py", "--port","80"]
